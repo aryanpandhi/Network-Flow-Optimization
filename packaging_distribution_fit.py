834fa6574a0773d23030827c9a617037def9618f
Aryan Pandhi <55639160+aryanpandhi@users.noreply.github.com>
2020-05-28 16:53:23 +0530

    Add files via upload

---
+++ packaging_distribution_fit.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import pandas as pd 
+from scipy.stats import norm
+from scipy.stats import kstest
+
+dfdata = pd.read_csv('packaging_data.csv')
+
+collectlist = []
+for k in range(0,50000,1000):
+    param = norm.fit(dfdata.iloc[k:k+1000,3])
+    test = kstest(dfdata.iloc[k:k+1000,3],'norm',args=param)
+    collectlist.append([dfdata.iloc[k,0],dfdata.iloc[k,1],dfdata.iloc[k,2],param[0],param[1],test[1]])
+
+df = pd.DataFrame(collectlist,columns=['Site','Size','Packaging Type','Processing Mean','Processing SD','kstest - p'])
+
+df.to_csv('packaging_statistics.csv')
---
+++ prefinish_distribution_fit.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import pandas as pd 
+from scipy.stats import norm
+from scipy.stats import kstest
+
+dfdata = pd.read_csv('prefinish_data.csv')
+
+collectlist = []
+for k in range(0,300000,1000):
+    param = norm.fit(dfdata.iloc[k:k+1000,3])
+    test = kstest(dfdata.iloc[k:k+1000,3],'norm',args=param)
+    collectlist.append([dfdata.iloc[k,0],dfdata.iloc[k,1],dfdata.iloc[k,2],param[0],param[1],test[1]])
+
+df = pd.DataFrame(collectlist,columns=['Site','Size','Flavor','Processing Mean','Processing SD','kstest - p'])
+
+df.to_csv('prefinish_statistics.csv')
+
+ 
